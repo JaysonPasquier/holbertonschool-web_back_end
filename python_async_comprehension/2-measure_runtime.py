e9e3078ae432bfdb26837ba3fa44f9152c64c246
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2025-04-28 09:23:51 +0200

    remove all the unnecessary doc and change most of the script

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python3
-"""
-Import async_comprehension from the previous file and write a
-measure_runtime coroutine that will execute async_comprehension
-four times in parallel using asyncio.gather.
-"""
-from asyncio import gather
-from time import time
+''' Python async comprehension '''
+import asyncio
+import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """
-    Coroutine that will execute async_comprehension four times in
-    parallel using asyncio.gather and returns the total runtime.
-    """
-    start_time = time()
-    await gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-    end_time = time()
-    return end_time - start_time
+    ''' unnecessary comment this is a self descriptive code papá'''
+    start = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end = time.time()
+    return end - start
