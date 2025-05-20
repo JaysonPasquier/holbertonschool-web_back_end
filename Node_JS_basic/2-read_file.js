af2f7d59c79603c3ac92e9052d89b0a394069277
Your Name <your.email@example.com>
2025-05-20 11:51:50 +0200

    task 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,30 +1,21 @@
 const fs = require('fs');
 
-/**
- * Counts the students in a CSV data file.
- * @param {String} path The path to the CSV data file.
- */
-function countStudents(path) {
+const countStudents = (aPath) => {
   try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
-    const studentLines = lines.slice(1);
-    console.log(`Number of students: ${studentLines.length}`);
-    const studentsByField = {};
-    studentLines.forEach(line => {
-      const [firstName, lastName, age, field] = line.split(',');
-      if (!studentsByField[field]) {
-        studentsByField[field] = [];
-      }
-      studentsByField[field].push(firstName);
-    });
-    for (const field in studentsByField) {
-      const students = studentsByField[field];
-      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    let data = fs.readFileSync(aPath, 'utf8').toString().split('\n');
+    data = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${data.length}`);
+    const subjects = {};
+    for (const row of data) {
+      const student = row.split(',');
+      if (!subjects[student[3]]) subjects[student[3]] = [];
+      subjects[student[3]].push(student[0]);
+    }
+    for (const subject in subjects) {
+      if (subject) console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
     }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
-}
-
+};
 module.exports = countStudents;
